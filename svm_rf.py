d0500c12d5752c3159694e3e45865185f82f46d9
lli27 <190072358@qq.com>
2018-08-16 11:20:42 +0800

    Update and rename test0.py to svm_rf.py

--- svm_rf.py
+++ svm_rf.py
@@ -11,20 +11,7 @@ from sklearn import svm
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-#fr=open('Concrete_Slump.txt')
-#data=[inst.strip().split(',') for inst in fr.readlines()]
-#for i in range(len(data)):
-#    data[i]=[float(x) for x in data[i]]
-#data=np.mat(data)
-#X=data[:,1:8]
-#Y=data[:,8]
-#fr=open('baseball.txt')
-#data=[inst.strip().split(',') for inst in fr.readlines()]
-#for i in range(len(data)):
-#    data[i]=[float(x) for x in data[i]]
-#data=np.mat(data)
-#X=data[:,:-1]
-#Y=data[:,-1]
+
 fr=open('delta_elv.txt')
 data=[inst.strip().split(',') for inst in fr.readlines()]
 for i in range(len(data)):
@@ -44,8 +31,8 @@ for i in range(100):
     scaler_y=MinMaxScaler(feature_range=(0,1))
     Y_train=scaler_y.fit_transform(Y_train)
     Y_test=scaler_y.transform(Y_test)
-    clf=RandomForestRegressor(n_estimators=100)#40,60,50,100
-    #clf=svm.SVR(gamma=10**(-2))#-0.5,-2,-0.5,-2
+    clf=RandomForestRegressor(n_estimators=100)
+    #clf=svm.SVR(gamma=10**(-2))
     Y_train=np.ravel(Y_train)
     Y_test=np.ravel(Y_test)
     clf.fit(X_train,Y_train)
@@ -60,7 +47,7 @@ for i in range(100):
     err_mean=np.ones_like(err)*np.mean(err)
     err_var=(((err-err_mean)**2).sum())/(n-1)
     y_var=(((Y_test-Y_mean)**2).sum())/(n-1)
-    Evar=1-err_var/y_var #0.9818
+    Evar=1-err_var/y_var
    #print('mse_test=',mse_test,'\n r2=',r2,'\n Evar=',Evar)
     a=[mse_train,mse_test,r2,Evar]
     result.append(a)
