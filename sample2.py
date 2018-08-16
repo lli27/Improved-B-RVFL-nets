887c678f919008c7269a9a9d09ccb85709c625be
lli27 <190072358@qq.com>
2018-08-16 11:14:41 +0800

    Update sample2.py

--- sample2.py
+++ sample2.py
@@ -16,42 +16,7 @@ for i in range(len(data)):
 data=np.mat(data)
 X=data[:,:-1]
 Y=data[:,-1]
-#fr=open(r'D:/python3_work/Concrete_Slump.txt')
-#data=[inst.strip().split(',') for inst in fr.readlines()]
-#for i in range(len(data)):
-#    data[i]=[float(x) for x in data[i]]
-#data=np.mat(data)
-#X=data[:,1:8]
-#Y=data[:,8]
-#fr=open(r'D:/python3_work/ele1.txt')
-#data=[inst.strip().split(',') for inst in fr.readlines()]
-#for i in range(len(data)):
-#    data[i]=[float(x) for x in data[i]]
-#data=np.mat(data)
-#X=data[:,:-1]
-#Y=data[:,-1]
-#fr=open(r'D:\python3_work\Abalone.txt')
-#data=[inst.strip().split(',') for inst in fr.readlines()]
-#for i in range(len(data)):
-#    data[i]=[float(x) for x in data[i]]
-#data=np.mat(data)
-#X=data[:,:-1]
-#Y=data[:,-1]
-#data=np.loadtxt(r'D:\python3_work\airfoil.txt')#0.5853
-#X=data[:,:-1]
-#Y=data[:,-1]
-#data=xlrd.open_workbook('Concrete.xls')
-#data=data.sheets()[0]
-#X=np.zeros((1030,8))
-#for i in range(8):
-#    X[:,i]=data.col_values(i)
-#Y=data.col_values(8)
 Y=np.reshape(Y,(len(Y),1))
-#split train,test set
-#X_train,X_test,Y_train,Y_test=train_test_split(X,Y,random_state=1)
-#10-kfold cross-validation for train set
-#XX=X_train
-#YY=Y_train
 lamda=10**(np.linspace(0,1,3))
 temp=1000
 for i in range(len(lamda)):
@@ -71,11 +36,3 @@ for i in range(len(lamda)):
 print('\nbest lamda value=\n',lamda_)
 print('\nmean=\n',mm)
 print('\nstd=\n',ss)
-#result=[];lamda_=10**(0.5)
-#for i in range(100):
-#	result.append(BRR.BRR(X_train,Y_train,X_test,Y_test,lamda_))
-#result=pd.DataFrame(result)
-#print('\nmean=\n',np.mean(result))
-#print('\nstd=\n',np.std(result))
-
-
